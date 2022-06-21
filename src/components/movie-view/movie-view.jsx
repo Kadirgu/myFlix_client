9dc6e47aac708e83802a6e6d6ffb6c7ba6aaf8c3
Kadirgu <Kadirgu@users.noreply.github.com>
2022-06-21 11:56:14 +0200

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,41 +1,58 @@
-import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import PropTypes from "prop-types";
+import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export class MovieView extends React.Component {
-
     keypressCallback(event) {
         console.log(event.key);
     }
 
     componentDidMount() {
-        document.addEventListener('keypress', this.keypressCallback);
+        document.addEventListener("keypress", this.keypressCallback);
     }
 
     componentWillUnmount() {
-        document.removeEventListener('keypress', this.keypressCallback);
+        document.removeEventListener("keypress", this.keypressCallback);
     }
 
     render() {
         const { movie, onBackClick } = this.props;
 
         return (
-            <Row className="main-view justify-content-md-center">
-                {selectedMovie
-                    ? (
-                        <Col md={8}>
-                            <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                        </Col>
-                    )
-                    : movies.map(movie => (
-                        <Col md={3}>
-                            <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                        </Col>
-                    ))
-                }
-            </Row>
+            <Container fluid className='moviesContainer'>
+                <Row>
+                    <Col>
+                        <div className='movie-poster'>
+                            <img src={movie.ImagePath} />
+                        </div>
+                        <div className='movie-title'>
+                            <span className='label'>Title: </span>
+                            <span className='value'>{movie.Title}</span>
+                        </div>
+                        <div className='movie-description'>
+                            <span className='label'>Description: </span>
+                            <span className='value'>{movie.Description}</span>
+                        </div>
+                        <div className='movie-genre'>
+                            <span className='label'>Genre: </span>
+                            <Link to={`/genres/${movie.Genre.Name}`}>
+                                <Button variant='link'>{movie.Genre.Name}</Button>
+                            </Link>
+                        </div>
+                        <div className='movie-director'>
+                            <span className='label'>Director: </span>
+                            <Link to={`/directors/${movie.Director.Name}`}>
+                                <Button variant='link'>{movie.Director.Name}</Button>
+                            </Link>
+                        </div>
+                        <Button variant='outline-secondary' onClick={() => onBackClick()}>
+                            Back
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
         );
-
     }
 }
 
